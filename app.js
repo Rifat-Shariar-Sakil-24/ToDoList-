e43c360d038855c3f13c7655de77a44e5d783b2b
Rifat Shariar Sakil <rifatsakilmi1309@gmail.com>
2023-07-05 00:40:45 +0600

    dependencies check

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+
+//create
+app.post('/insert', (request, response) => {
+
+});
+
+
+//read
+app.get('/getAll', (request, response)=>{
+   
+   console.log('test');
+});
+
+app.listen(process.env.PORT, () => console.log('app is running'));
--- index.js
+++ index.js
@@ -1,37 +1,15 @@
-var con = require('./connection');
-// var express = require('express');
-// var app = express();
+const { response } = require("express");
 
-// var bodyParser = require('body-parser');
-
-// app.use(bodyParser.json());
-
-// app.use(bodyParser.urlencoded({extended:true}));
-
-// app.get('/', function(req, res){
-
-//     res.sendFile(__dirname+'/register.html');
-
-// });
-
-// app.post('/', function(req,res){
-//  var name = req.body.name;
- con.connect(function(error){
-   if(error) throw error;
-   //var sql = "INSERT INTO notes(name) VALUES('"+name+"')";
-   //con.query(sql,function(error, result){
-
-   // if(error) throw error;
-   // res.send('Text inserted'+result.insertID);
-   con.query("select * from doctor", function(error, result){
-    if(error) throw error;
-    console.log(result);
-   })
-   console.log(44);
+document.addEventListener('DOMContentLoaded',function(){
    
- 
+    loadHTMLTable([]);
 });
 
+function loadHTMLTable(data){
+    const table = document.querySelector('table tbody');
 
+    if(data.length == 0){
+ table.innerHTML = "<tr> <td class = 'no-data' colspan = '5'> No Data </td> </tr>";
+    }
 
-
+}
